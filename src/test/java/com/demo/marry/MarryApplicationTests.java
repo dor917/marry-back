20f96f8dbc7795901df39248d6cd68595721f42d
김돈하 <donha917@naver.com>
2024-01-30 20:17:47 +0900

    init project

---
+++ src/test/java/com/demo/marry/MarryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.marry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
